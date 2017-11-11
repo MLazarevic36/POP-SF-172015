e934f4f71517bb3f5d500306b62ef440f82edfb2
Hrle <hrle96ab@gmail.com>
2017-11-11 15:51:25 +0100

    modelovanje objekata

---
+++ POP-SF172015/POP-SF172015/Model/Salon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POP_SF172015.Model
+{
+    class Salon
+    {
+        public int ID { get; set; }
+        public String Naziv { get; set; }
+        public String Adresa { get; set; }
+        public int Telefon { get; set; }
+        public String Email { get; set; }
+        public String AdresaSajta { get; set; }
+        public int PIB { get; set; }
+        public int MaticniBroj { get; set; }
+        public int BrZiroRacuna { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
