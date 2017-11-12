ad04be039196ee2344566a74b92d762cea0fc620
Hrle <hrle96ab@gmail.com>
2017-11-13 00:58:04 +0100

    rad na glavnom prozoru

---
+++ POP_SF172015WPF/POP_SF172015WPF/UI/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using POP_SF172015WPF.Model;
+using POP_SF172015WPF.UI;
+using POP_SF172015WPF.Menadzeri;
+
+namespace POP_SF172015WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void DodajNamestaj(object sender, RoutedEventArgs e)
+        {
+            var noviNamestaj = new Namestaj();
+            {
+                Naziv = ""
+            };
+            var namestajProzor = new NamestajWindow(noviNamestaj, NamestajWindow.Operacija.DODAVANJE);
+            namestajProzor.Show();
+        }
+
+        private void IzmeniNamestaj(object sender, RoutedEventArgs e)
+        {
+            var selektovaniNamestaj = (Namestaj)listBoxNamestaj.SelectedItem;
+            var namestajProzor = new NamestajWindow(selektovaniNamestaj, NamestajWindow.Operacija.IZMENA);
+            namestajProzor.Show();
+        }
+
+
+        private void Izlaz(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+
+    }
+}
+
--- POP_SF172015WPF/POP_SF172015WPF/UI/NamestajWindow.xaml.cs
+++ POP_SF172015WPF/POP_SF172015WPF/UI/NamestajWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using POP_SF172015WPF.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using POP_SF172015WPF.Menadzeri;
 
 namespace POP_SF172015WPF.UI
 {
@@ -19,9 +21,59 @@ namespace POP_SF172015WPF.UI
     /// </summary>
     public partial class NamestajWindow : Window
     {
-        public NamestajWindow()
+        private Namestaj namestaj;
+        private Operacija operacija;
+
+        public enum Operacija
+        {
+            DODAVANJE,
+            IZMENA
+        };
+    
+        public NamestajWindow(Namestaj namestaj, Operacija operacija)
         {
             InitializeComponent();
+
+            InicijalizujVrednosti(namestaj, operacija);
+
+
+        }
+
+        private void InicijalizujVrednosti(Namestaj namestaj, Operacija operacija)
+        {
+            this.namestaj = namestaj;
+            this.operacija = operacija;
+
+            tbNaziv.Text = namestaj.Naziv;
+        }
+        private void SacuvajIzmene(object)
+        {
+            List<Namestaj> postojeciNamestaj = Projekat.Instance.Namestaj;
+
+            switch (operacija)
+            {
+
+                case Operacija.DODAVANJE:
+                    var noviNamestaj = new Namestaj()
+                    {
+                        Naziv = tbNaziv.Text
+                    };
+                    postojeciNamestaj.Add(noviNamestaj);
+                    break;
+                case Operacija.IZMENA:
+                    foreach (var n in postojeciNamestaj)
+                    {
+                        if (n.Id == namestaj.Id)
+                        {
+                            n.Naziv = tbNaziv.Text;
+                            break;
+                        }
+                    }
+                    break;
+            }
+            Projekat.Instance.Namestaj = postojeciNamestaj;
+            this.Close();
         }
     }
 }
+
