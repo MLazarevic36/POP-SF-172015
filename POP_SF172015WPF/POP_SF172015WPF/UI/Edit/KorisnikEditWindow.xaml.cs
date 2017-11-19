eb4b279cb9bd14fd3df24d6e23080d4307e4fd19
Hrle <hrle96ab@gmail.com>
2017-11-19 14:16:47 +0100

    premestanje direktiva + u izradi prozori za korisnika

---
+++ POP_SF172015WPF/POP_SF172015WPF/UI/Edit/KorisnikEditWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using POP_SF172015WPF.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using POP_SF172015WPF.Menadzeri;
+
+namespace POP_SF172015WPF.UI.Edit
+{
+    /// <summary>
+    /// Interaction logic for KorisnikEditWindow.xaml
+    /// </summary>
+    public partial class KorisnikEditWindow : Window
+    {
+        Korisnik korisnik;
+        public enum Operacija
+        {
+            DODAVANJE,
+            IZMENA
+        };
+        Operacija operacija;
+
+        public KorisnikEditWindow()
+        {
+            InitializeComponent();
+            this.korisnik = korisnik;
+            this.operacija = operacija;
+        }
+
+        if (operacija == Operacija.DODAVANJE)
+        {
+            tbId.Text = (Projekat.ListaKorisnika.Count + 1).ToString();
+            tbTipKorisnika
+        }
+
+        private void btnPotvrdi_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
