6ee2dc9b2e23aa319c6ae965a13be069644f19b1
Hrle <hrle96ab@gmail.com>
2017-12-30 10:47:18 +0100

    ubacivanje tabela

--- POP_SF172015WPF/POP_SF172015WPF/Db skripte/crebas.sql
+++ POP_SF172015WPF/POP_SF172015WPF/Db skripte/crebas.sql
@@ -30,3 +30,36 @@ CREATE TABLE Akcija (
 	Obrisan BIT
 )
 GO
+
+CREATE TABLE Korisnici (
+
+	Id INT PRIMARY KEY IDENTITY(1, 1),
+	KorIme VARCHAR(20),
+	Lozinka VARCHAR(100),
+	Ime VARCHAR(20),
+	Prezime VARCHAR(100),
+	TipKorisnika VARCHAR(20),
+	Obrisan BIT
+
+)
+GO
+
+CREATE TABLE Racuni (
+
+	Id INT PRIMARY KEY IDENTITY(1, 1),
+	Kupac VARCHAR(100),
+	BrojRacuna INT not null,
+	UkupnaCena INT not null,
+	DatumProdaje DATE,
+	Obrisan BIT
+)
+GO
+
+CREATE TABLE DodatneUsluge (
+
+	Id INT PRIMARY KEY IDENTITY(1, 1),
+	Naziv VARCHAR(30),
+	Cena INT not null,
+	Obrisan BIT
+)
+GO
--- POP_SF172015WPF/POP_SF172015WPF/Db skripte/seed.sql
+++ POP_SF172015WPF/POP_SF172015WPF/Db skripte/seed.sql
@@ -12,3 +12,11 @@ VALUES (3, 2, 'Ugaoni kauc', 323.5, 16, 0);
 INSERT INTO Akcija(Popust, DatumPocetka, DatumZavrsetka, Obrisan) VALUES (15, '2018-3-2', '2018-6-2', 0);
 INSERT INTO Akcija(Popust, DatumPocetka, DatumZavrsetka, Obrisan) VALUES (20, '2018-4-6', '2018-7-6', 0);
 INSERT INTO Akcija(Popust, DatumPocetka, DatumZavrsetka, Obrisan) VALUES (18, '2018-8-7', '2018-11-7', 0);
+
+
+INSERT INTO Korisnici(KorIme, Lozinka, Ime, Prezime, TipKorisnika, Obrisan)
+VALUES ('abc', '123', 'Gordon', 'Ramsay', 'ADMIN', 0);
+INSERT INTO Korisnici(KorIme, Lozinka, Ime, Prezime, TipKorisnika, Obrisan)
+VALUES ('abcd', '1234', 'Alex', 'Jones', 'PRODAVAC', 0);
+INSERT INTO Korisnici(KorIme, Lozinka, Ime, Prezime, TipKorisnika, Obrisan)
+VALUES ('fgh', '789', 'Tesa', 'Tesanovic', 'ADMIN', 0);
