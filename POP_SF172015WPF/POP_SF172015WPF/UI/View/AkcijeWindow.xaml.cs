9cd4c9fffe36c3bd326967b89c1c6207b1eb5e15
Hrle <hrle96ab@gmail.com>
2017-12-30 15:49:08 +0100

    menjanje naslova na svim prozorima

--- POP_SF172015WPF/POP_SF172015WPF/UI/View/AkcijeWindow.xaml.cs
+++ POP_SF172015WPF/POP_SF172015WPF/UI/View/AkcijeWindow.xaml.cs
@@ -1,19 +1,9 @@
 ﻿using POP_SF172015WPF.Model;
 using POP_SF172015WPF.UI.Edit;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace POP_SF172015WPF.UI
 {
@@ -35,13 +25,6 @@ namespace POP_SF172015WPF.UI
             dgAkcije.ColumnWidth = new DataGridLength(1, DataGridLengthUnitType.Star);
         }
 
-        private void dgAkcije_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
-        {
-            if ((string)e.Column.Header == "Id")
-            {
-                e.Cancel = true;
-            }
-        }
 
         private void btnDodaj_Click(object sender, RoutedEventArgs e)
         {
@@ -69,6 +52,21 @@ namespace POP_SF172015WPF.UI
 
         private void btnObrisi_Click(object sender, RoutedEventArgs e)
         {
+
+        }
+
+        private void dgAkcije_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
+        {
+
+            if ((string)e.Column.Header == "Id")
+            {
+                e.Cancel = true;
+            }
+
+            if ((string)e.Column.Header == "Obrisan")
+            {
+                e.Cancel = true;
+            }
 
         }
     }
